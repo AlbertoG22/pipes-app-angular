c38a3f681681a97701ba73288a43412c3a86f472
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-06-05 21:39:36 -0600

    changing date language in pipe date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
+// Configuración del 'locale' de la app
+import localeEsMX from '@angular/common/locales/es-MX';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsMX );
+registerLocaleData( localeFrCA );
 
 @NgModule({
   declarations: [
