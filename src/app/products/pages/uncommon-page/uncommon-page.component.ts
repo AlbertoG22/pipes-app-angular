6b336d9da916f253dd0f6e04ede78e19d19a97ab
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-06-12 18:12:21 -0600

    async pipe (with observable)

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -38,4 +39,10 @@ export class UncommonPageComponent {
     age: 25,
     address: 'Colima, México'
   };
+
+  // Async Pipe
+  public myObservableTimer = interval(2000).pipe(
+    tap( value => console.log('tap: ', value) )
+  );
+
 }
