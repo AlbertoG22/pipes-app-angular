91e2632ceb4dac8598f9860127b5c6284ae1d197
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-06-15 21:27:30 -0600

    sort table data with a pipe

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -8,6 +8,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero;
 
   public heroes: Hero[] = [
     {
@@ -41,5 +42,7 @@ export class OrderComponent {
     this.isUpperCase = !this.isUpperCase;
   }
 
-
+  changeOrder( value: keyof Hero ) {
+    this.orderBy = value;
+  }
 }
